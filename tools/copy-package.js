8a20d0a1df64c58e17778017ae296f41e1fd1617
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2018-04-07 18:17:19 -0500

    updated package.json and copy readme

--- tools/copy-package.js
+++ tools/copy-package.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
 let resizable = fs.readFileSync('package.json').toString();
+let readme = fs.readFileSync('readme.md').toString();
 
 fs.writeFileSync('dist/package.json', resizable);
+fs.writeFileSync('dist/readme.md', readme);
